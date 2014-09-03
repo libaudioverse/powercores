2b29fb0a072b0f868ec56f5373c016b633944120
Austin Hicks <camlorn38@gmail.com>
2014-09-02 20:34:30 -0400

    Added missing pragma once

--- include/lambdatask/monitor.hpp
+++ include/lambdatask/monitor.hpp
@@ -1,5 +1,6 @@
 /**This file is part of Lambdatask, released under the terms of the Unlicense.
 See LICENSE in the root of the Lambdatask repository for details.*/
+#pragma once
 #include <mutex>
 #include <memory>
 #include <utility>
