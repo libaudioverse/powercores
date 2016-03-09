2018ab3a5fe2b59f61d984995344d5d365f7df41
Austin Hicks <camlorn@camlorn.net>
2016-03-08 22:03:43 -0500

    Another gcc-related workaround.

--- include/powercores/thread_pool.hpp
+++ include/powercores/thread_pool.hpp
@@ -91,7 +91,7 @@ class ThreadPool {
 	The function receives the result of dereferencing the iterator and any additional arguments, and will run in some unspecified order.  The iterators must be random access.*/
 	template<typename CallableT, typename IterT, typename... ArgsT>
 	void map(CallableT &&callable, IterT begin, IterT end, ArgsT&&... args) {
-		auto executor = [callable, args...](IterT subrangeBegin, IterT subrangeEnd) {
+		auto executor = [callable = callable, args...](IterT subrangeBegin, IterT subrangeEnd) {
 			for(; subrangeBegin != subrangeEnd; subrangeBegin++) callable(*subrangeBegin, args...);
 		};
 		int amount = end-begin;
